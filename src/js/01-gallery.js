b4cdf952e279729bd9a7a8e7216a86f7a7745af4
Rumberg Vitalii <modtitiof@gmail.com>
2023-09-13 13:32:01 +0300

    done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,61 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const refs = {
+    galleryList: document.querySelector('.js-gallery'),
+};
 
-console.log(galleryItems);
+function galleryMarkup(array) {
+  const markup = array
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+
+  return markup;
+}
+
+function renderGalleryList(galleryArr) {
+  const markup = galleryMarkup(galleryArr);
+    refs.galleryList.insertAdjacentHTML('beforeend', markup);
+}
+renderGalleryList(galleryItems);
+
+refs.galleryList.addEventListener('click', onGalleryListClick);
+
+let imageBox;
+const onBoxShow = () => {
+    document.addEventListener('keydown', onDocumentKeyDown);
+  
+}
+const onBoxClose = () => {
+    document.removeEventListener('keydown', onDocumentKeyDown);
+  
+}
+
+function onGalleryListClick(event) {
+    event.preventDefault();
+    if (event.target === event.currentTarget) return;
+    const imgValue = event.target;
+    const urlValue = imgValue.dataset.source;
+    imageBox = basicLightbox.create(`<img width="1400" height="900" src="${urlValue}">`, {
+        onShow: onBoxShow,
+        onClose: onBoxClose
+    });
+    imageBox.show();
+}
+
+function onDocumentKeyDown(event) {
+    if (event.key === 'Escape') { 
+        imageBox.close();
+    }   
+};
+
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const refs = {
+    galleryList: document.querySelector('.js-gallery'),
+};
 
-console.log(galleryItems);
+function galleryMarkup(array) {
+  const markup = array
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    })
+    .join('');
+
+  return markup;
+}
+
+function renderGalleryList(galleryArr) {
+  const markup = galleryMarkup(galleryArr);
+    refs.galleryList.insertAdjacentHTML('beforeend', markup);
+}
+renderGalleryList(galleryItems);
+
+let gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250, });
